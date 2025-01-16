23d5eae1b3e8bb2ed03039d58a501985a9d9545d
gomaiwun <gomaiwun@gmail.com>
2025-01-15 21:09:10 -0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,28 @@
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
+def test() {
+    echo "Testing the application"
+    echo "Executing pipeline for branch $BRANCH_NAME"
 }
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+def buildJar() {
+    echo "Building JAR..."
+    sh "mvn package"
+    echo "JAR build complete"
+}
+
+def buildAndPushImage() {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        echo "Building and pushing Docker image..."
+        sh "docker build -t gomaiwun/demo-app:jma-2.0 ."
+        sh "docker push gomaiwun/demo-app:jma-2.0"
+        echo  "Image build and push complete"
     }
 }
 
 def deployApp() {
-    echo 'deploying the application...'
+    echo "Deploying application..."
+    // Actual deployment command
+    // sh "your_deployment_command"
+    echo "Deployment complete"
 }
 
 return this
